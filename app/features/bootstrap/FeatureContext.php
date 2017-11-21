03475877ab28b812592c228de7b595054a0308ec
Dimitri Sandron <d.sandron@i-akademy.fr>
2017-11-21 14:53:52 +0100

    FeatureContext généré

--- app/features/bootstrap/FeatureContext.php
+++ app/features/bootstrap/FeatureContext.php
@@ -18,34 +18,5 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
-
     }
-
-    /**
-     * @Given I am on the task page list
-     */
-    public function anEmptyBasket()
-    {
-        
-    }
-
-    /**
-     * @When  I click the Add new task button
-     */
-    public function aProductCostingIsAddedToTheBasket($arg1)
-    {
-        $product = Product::withCost($arg1);
-        $this->basket->addProduct($product);
-    }
-
-    /**
-     * @Then Go to the Task Create page
-     */
-    public function theBasketPriceIs($arg1)
-    {
-        if($this->basket->price() != $arg1){
-            throw new Exception('Le prix ne correspond pas');
-        }
-    }
-
 }
