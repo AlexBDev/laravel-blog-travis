26eea64e194d28c665df235382d1d880caed35af
Alexis Bussieres <bussieres.alexis.dev@gmail.com>
2017-11-23 13:21:40 +0100

    Change deploy branch to master

--- app/.rocketeer/scm.php
+++ app/.rocketeer/scm.php
@@ -21,7 +21,7 @@ return [
     'password'   => null,
 
     // The branch to deploy
-    'branch'     => 'setup/rocketeer',
+    'branch'     => 'master',
 
     // Whether your SCM should do a "shallow" clone of the repository
     // or not – this means a clone with just the latest state of your
